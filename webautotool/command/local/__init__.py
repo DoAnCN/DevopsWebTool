334b8a894367ee1f3468430e2c0629cec85c5fff
Dat, Ly Hong <dat.lh@komit-consulting.com>
2018-05-23 23:40:30 +0700

    Add new command clone project on local

--- webautotool/command/local/__init__.py
+++ webautotool/command/local/__init__.py
@@ -1,10 +1,25 @@
 # -*- coding:utf-8 -*-
 
 import click
+import sh
 from webautotool.commmand import webautotool as cli
+from webautotool.config.log import logger
 
-@cli.command
+@cli.group()
+@click.pass_context
+def web(ctx, *args, **kw):
+    """
+    Command use on local
+    """
+    if ctx.invoked_subcommand is None:
+        ctx.invoke(start, *args, **kw)
+
+@web.command
 @click.argument('url')
+@click.option
 @click.pass_context
 def start(url):
-    print("")
+    log = logger("start")
+    log.info("Checking url")
+    sh.git('ls-remote', url)
+    
