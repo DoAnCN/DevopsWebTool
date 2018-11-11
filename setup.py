4bf38cf73aed645bab8070b3230e2b1b08ce958d
datlh <lydat143@gmail.com>
2018-11-11 11:20:29 +0700

    Add format file .manager and auto copy that file to system when install

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 from webautotool import __version__
+
+class createLoginFile(install):
+    def run(self):
+        # with open('manager.j2') as f: content = f.read()
+        path = os.environ['HOME']
+        print("=====================")
+        os.system('echo \"{0}\" > {1}/.manager'.format("ffffffffffffffffffff", path))
+        install.run(self)
 
 setup(
     name='webautotool',
@@ -10,6 +20,7 @@ setup(
     author_email=['14520143@gm.email.com', '14520862@gm.uit.edu.vn'],
     description='Usual development commands used to manage websites are '
                 'promgrammed by PHP',
+    cmdclass={'install': createLoginFile},
     packages=find_packages(),
     install_requires=[
         'click>=6.0',
